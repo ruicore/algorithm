514085f919d4737e2b17d7f154bb47a27c3b9b5e
HeRuivio <super76rui@icloud.com>
2018-08-23 15:53:01 +0800

    2018-08-23 修改直接插入排序，比较一次，移动一次

--- 2018-08-23-Direct-Insertion-Sort.py
+++ 2018-08-23-Direct-Insertion-Sort.py
@@ -14,27 +14,13 @@ def DirectInsertionSort():
     print("%-*s" % (15, "UnSorted List"), number_list)
     length = len(number_list)
     for index in range(1, length):
-        for before in range(index-1, 0, -1):
-            if number_list[index] < number_list[before] and number_list[index] >= number_list[before-1]:
-                temp = number_list[index]
-                DirectInsertionSortMove(number_list, before, index-1)
-                number_list[before] = temp
-                break
-            if before == 1 and number_list[index] < number_list[0]:
-                temp = number_list[index]
-                DirectInsertionSortMove(number_list, before, index-1)
-                number_list[before] = temp
-                break
+        before = index-1
+        compare = number_list[index]
+        while before >= 0 and compare <= number_list[before]:
+            number_list[before+1] = number_list[before]
+            number_list[before] = compare
+            before -= 1
     print("%-*s" % (15, "Sorted List"), number_list)
-
-
-def DirectInsertionSortMove(numbers, start, end):
-    """
-    把numbers数组编号[start,end]的数一次往后移动一个
-    """
-    for index in range(end, start-1, -1):
-        numbers[index+1] = numbers[index]
-    return None
 
 
 if __name__ == "__main__":
