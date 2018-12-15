8be7c3019a0b9c48e66432face154b0fac154138
HeRuivio <super76rui@icloud.com>
2018-12-15 18:49:08 +0800

    2018-12-15 55. Jump Game

--- Leetcode/2018-12-15-55-Jump-Game.py
+++ Leetcode/2018-12-15-55-Jump-Game.py
@@ -11,15 +11,21 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
+        # 记录最远位置
         maxindex = 0
         for i in range(len(nums)):
-            if i+nums[i] > maxindex and maxindex >= i:
+            # 如果能偶到达当前位置并且当前位置能够到达的最远位置大于maxindex
+            if maxindex >= i and i+nums[i] > maxindex:
+                # 更新maxindex
                 maxindex = i+nums[i]
+            # 一旦maxindex大于等于最大索引，我们返回False
             if maxindex >= len(nums)-1:
                 return True
+        # 如果到末尾maxinde都小于最大索引，返回False
         return False
+
 
 if __name__ == "__main__":
     so = Solution()
-    res = so.canJump([3,2,1,0,4])
+    res = so.canJump([3, 2, 1, 0, 4])
     print(res)
