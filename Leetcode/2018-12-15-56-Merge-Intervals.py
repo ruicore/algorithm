82f610b37a54d1fa3e05351ec74ea08ee6624632
HeRuivio <super76rui@icloud.com>
2018-12-16 15:19:25 +0800

    2018-12-16 56. Merge Intervals

--- Leetcode/2018-12-15-56-Merge-Intervals.py
+++ Leetcode/2018-12-15-56-Merge-Intervals.py
@@ -15,23 +15,32 @@ class Interval:
 
 class Solution:
     def depart(self, intervals, left, right):
-        pivotindex = left+((right-left) >> 1)
-        pivotvalue = intervals[pivotindex].start
+        # 确定分区点的位置
+        pivot = left+((right-left) >> 1)
+        # 将分区点和最后一个位置交换
+        intervals[right], intervals[pivot] = intervals[pivot], intervals[right]
+        # i，j 初始位置为最左边的位置，i表示以排序区间的末尾（不包含在排序区间之内）
         i, j = left, left
-        for j in range(left, right+1):
-            if intervals[j].start < pivotvalue:
+        for j in range(left, right):
+            # 如果当前位置比pivot位置的值小（由于已经把pivot交换到最后一个位置，pivot==riht）
+            if intervals[j].start < intervals[right].start:
+                # 把当前位置的值放入已排序区间的末尾
                 intervals[i], intervals[j] = intervals[j], intervals[i]
-                if i == pivotindex:
-                    pivotindex = j
+                # 已排序区间自增一次
                 i += 1
-        intervals[i], intervals[pivotindex] = intervals[pivotindex], intervals[i]
+        # 把最后一个位置和i交换
+        intervals[i], intervals[right] = intervals[right], intervals[i]
         return i
 
     def quicksort(self, intervals, left, right):
+        # 递归结束条件，当left>=right时
         if left >= right:
             return
+        # 对数组取某一个值，分为两块
         middle = self.depart(intervals, left, right)
+        # 对左边排序
         self.quicksort(intervals, left, middle-1)
+        # 对右边排序
         self.quicksort(intervals, middle+1, right)
 
     def merge(self, intervals):
@@ -40,7 +49,9 @@ class Solution:
         :rtype: List[Interval]
         """
         ans = []
+        # 对
         self.quicksort(intervals, 0, len(intervals)-1)
+        # 这里也可以用python自带的排序函数，python排序函数是用C实现的，速度更快
         # intervals = sorted(intervals, key=lambda x: x.start)
         for item in intervals:
             if not ans or item.start > ans[-1].end:
@@ -52,10 +63,10 @@ class Solution:
 
 if __name__ == "__main__":
     so = Solution()
-    a = Interval(1, 4)
-    b = Interval(0, 4)
+    a = Interval(1, 3)
+    b = Interval(2, 6)
     c = Interval(8, 10)
-    # d = Interval(15, 18)
-    res = so.merge([a, b])
-    # for item in res:
-    #     print(item.start, item.end)
+    d = Interval(15, 18)
+    res = so.merge([a, b, c, d])
+    for item in res:
+        print(item.start, item.end)
