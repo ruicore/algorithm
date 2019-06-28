85dca6fa052be0869faa1313fd3ee3ebbde3ec47
ruicore <super76rui@icloud.com>
2019-06-28 16:33:05 +0800

    2019-06-28 374. Guess Number Higher or Lower

---
+++ LeetCode/2019-06-28-374-Guess-Number-Higher-or-Lower.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author:             何睿
+# @Create Date:        2019-06-28 16:24:44
+# @Last Modified by:   何睿
+# @Last Modified time: 2019-06-28 16:29:54
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        
+        num = left + ((right - left) >> 1)
+        res = guess(num)
+        if res == 0: return num
+
+        while res:
+            if res == 1:
+                left = num + 1
+            if res == -1:
+                right = num - 1
+            num = left + ((right - left) >> 1)
+            res = guess(num)
+
+        return num
+                
